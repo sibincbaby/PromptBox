083a13c5db0b8c8cd875a5e63d6da509f75ebc77
sibinc <sibin@linways.com>
2025-05-12 11:33:20 +0530

    feat: hide default template edits

--- src/views/TemplatesPage.vue
+++ src/views/TemplatesPage.vue
@@ -85,37 +85,37 @@
               
               <!-- Action Area -->
               <div class="flex mt-3 pt-2 border-t border-gray-100">
-                <button @click="viewTemplate(template.id)" 
-                      class="flex-1 py-1 text-sm text-gray-600 flex items-center justify-center transition-transform active:scale-95">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                  </svg>
-                  View
-                </button>
-                
-                <div class="w-px h-6 bg-gray-100 self-center"></div>
-                
-                <button @click="editTemplate(template.id)"
-                      class="flex-1 py-1 text-sm text-indigo-600 flex items-center justify-center transition-transform active:scale-95">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                  </svg>
-                  Edit
-                </button>
-                
-                <div class="w-px h-6 bg-gray-100 self-center"></div>
-                
-                <button @click="confirmDelete(template.id)"
-                      class="flex-1 py-1 text-sm text-red-500 flex items-center justify-center transition-transform active:scale-95"
-                      :class="{ 'opacity-50 cursor-not-allowed': template.isDefault }"
-                      :disabled="template.isDefault"
-                      :title="template.isDefault ? 'Default templates cannot be deleted' : 'Delete template'">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                  </svg>
-                  Delete
-                </button>
+                <!-- Only show action buttons for non-default templates -->
+                <template v-if="!template.isDefault">
+                  <button @click="viewTemplate(template.id)" 
+                        class="flex-1 py-1 text-sm text-gray-600 flex items-center justify-center transition-transform active:scale-95">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                    </svg>
+                    View
+                  </button>
+                  
+                  <div class="w-px h-6 bg-gray-100 self-center"></div>
+                  
+                  <button @click="editTemplate(template.id)"
+                        class="flex-1 py-1 text-sm text-indigo-600 flex items-center justify-center transition-transform active:scale-95">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                    </svg>
+                    Edit
+                  </button>
+                  
+                  <div class="w-px h-6 bg-gray-100 self-center"></div>
+                  
+                  <button @click="confirmDelete(template.id)"
+                        class="flex-1 py-1 text-sm text-red-500 flex items-center justify-center transition-transform active:scale-95">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                    </svg>
+                    Delete
+                  </button>
+                </template>
               </div>
             </div>
           </div>
